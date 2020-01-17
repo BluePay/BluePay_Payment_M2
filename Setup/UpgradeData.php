3a9a08cedc6982b56f8632ef53a3efe22561584d
Eric Margules <emargules@bluepay.com>
2020-01-17 14:21:00 -0600

    Add Upgrade script

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BluePay\Payment\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+	public function upgrade(
+		ModuleDataSetupInterface $setup,
+		ModuleContextInterface $context
+	) {
+		$setup->startSetup();
+		if (version_compare($context->getVersion(), '1.1.0', '<')) {
+			$connection = $setup->getConnection();
+			$table = $setup->getTable('quote_payment');
+			if ($connection->isTableExists($table) == true) {
+				$connection->query("UPDATE quote_payment SET `additional_information` = NULL WHERE `method` = 'bluepay_payment';");
+			}
+		}
+
+		$setup->endSetup();
+	}
+}
